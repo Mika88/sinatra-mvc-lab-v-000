f56f0f1a32dc2d00b226b50318d21e6af29a1fc9
Mika88 <mikaspivak@gmail.com>
2018-11-13 14:37:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,11 @@
 class PigLatinizer
+  def initialize(words)
+    @words = words
+  end
 
+  def piglatinize(words)
+    words.each do |word|
+      word.gsub(word[0], word[-1])
+    end
+  end 
 end
